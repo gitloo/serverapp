1049975686faa325cc07fe85ebe8c130274fd856
gitloo <lorenzo.silot@edu.unito.it>
2022-12-08 05:23:36 +0100

    Updated server.service & app.component See01:56:00

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { catchError } from 'rxjs';
+import { startWith } from 'rxjs';
+import { of } from 'rxjs';
+import { map } from 'rxjs';
+import { Observable } from 'rxjs';
+import { DataState } from './enum/data-state.enum';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { ServerService } from './service/server.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'serverapp';
+
+// we need to implement an interface OnInit, and because we are implementing it we're obligated to define the method ngOnInit().
+export class AppComponent implements OnInit {
+  // the entire state of application is going to be inside of this appState variable, and the type of it is an Observable,
+  // then we pass in the type for that Observable, which is the entire application state and the data we're going to manage is 
+  // the custom http response
+  appState$: Observable<AppState<CustomResponse>>;
+  
+  constructor(private serverService: ServerService) {}
+
+
+  // Every time that this component is done initializing, is going to call this ngOnInit() and that's gonna fire any code we put inside
+  ngOnInit(): void{
+    this.appState$ = this.serverService.servers$
+    .pipe(
+      map(response => {
+        // Whenever the app initialized it's gonna go through this and...
+
+        // 2 once the app get some data, it's gonna return LOADED state or...
+        return { dataState: DataState.LOADED_STATE, appData: response } // the object returned does not give any complaint because it matches the description of the type that we set for appState$
+      }),
+      // 1 if it doesn't have the response immediately, will call the startWith() method and show to the user that the app is in LOADING state, but
+      startWith({ dataState: DataState.LOADING_STATE }),
+      // 3 ...,if  we get an error for some reason, it's gonna return ERROR state.
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error })
+      })
+    );
+  }
 }
--- src/app/service/server.service.ts
+++ src/app/service/server.service.ts
@@ -11,7 +11,7 @@ import { Server } from '../interface/server';
   providedIn: 'root'
 })
 export class ServerService {
-  private readonly apiUrl: 'any';
+  private readonly apiUrl: 'http://localhost:8080/';
 
   constructor(private http: HttpClient) { }
    
@@ -33,9 +33,9 @@ export class ServerService {
     catchError(this.handleError)
   );
 
-  // save a server
+  // save a server = post request, we have to pass in the request body
   save$ = (server: Server) => <Observable<CustomResponse>>
-  this.http.get<CustomResponse>('${this.apiUrl}/server/save')
+  this.http.post<CustomResponse>('${this.apiUrl}/server/save', server)
   .pipe(
     tap(console.log),
     catchError(this.handleError)
@@ -49,19 +49,48 @@ export class ServerService {
     catchError(this.handleError)
   );
 
-  // to filter all the servers
-  delete$ = (status: Status, response: CustomResponse) => <Observable<CustomResponse>>
+  /** <b>filter all the servers by a specific status.</b>
+   *
+   * What we're getting, in terms of what parameters this new Observable is taking, is the status that we want to filter by in the data
+   * so the entire http response and what we're doing is :
+   * - creating the new Observable of type CustomResponse
+   * - define a callback function (=>) which return a suscriber (suscriber.complete();) after
+   *     1) console logging the response
+   *     2) calling .next(), the way we emit a new value to whoever is subscribed to this Observable
+   *        - for the object that we want to return, we're gonna check to see if the status is ALL. If this is the case,
+   *           we're going to return the first response, an object made by response and message. 
+   *        - If the status is not ALL, then we have to filter the servers so we're going to 
+   *            1) we spread everything inside the response (again),
+   *            2) override the message and the data that we have to filter. The message is going to be: we're gonna filter all the servers with .filter() and
+   *               if the lenght of the array after we filtered everything (because when .filter() is called, it's gonna filter the array based on the condtition 
+   *               inside the curved brakets) is > 0, than we're going to pass the message "Servers filtered by..." and SERVER_UP or SERVER_DOWN depending by the 
+   *               case or "No server of _status found",
+   *            3) data = the servers that we have to filter and return to the user. For data (which is an object from the interface CustomResponse) 
+   *               we're going to say: for the servers, we're just gonna get the response, so, whatever we got form the caller of this function, 
+   *               grab the data (the array of Server) and then access the servers, filtering by whatever status is passed.
+   *               
+   */
+  filter$ = (status: Status, response: CustomResponse) => <Observable<CustomResponse>>
   new Observable<CustomResponse>(
-    subscriber => {
+    suscriber => {
       console.log(response);
-      subscriber.next(
-        status == Status.ALL ? { ...response, message : 'Servers filtered by ${status} status'} :
+      suscriber.next(
+        status == Status.ALL ? 
+        { ...response, message : 'Servers filtered by ${status} status'} 
+        // or ..
+        :
         {
           ...response,
-          message: response.data.servers?
-          .filter(server => server.status === status).length > 0 ? 'Servers filtered by 
-          ${status === Status.SERVER_UP ? ' SERVER UP' : 'SERVER DOWN'} status'
+          // override message 
+          message: response.data.servers
+          .filter(server => server.status === status).length > 0 ? "Servers filtered by" +
+           + "${status === Status.SERVER_UP ? 'SERVER UP'" + 
+           + " : 'SERVER DOWN'} status" : "No server of ${status} found",
+           // to solve the error "response: CustomResponse is probably undefined _> tsconfig.jspn"
+           data: { servers: response.data.servers.filter(server => server.status === status)}
         }
+      );
+      suscriber.complete();
     }
   )
   .pipe(
@@ -79,7 +108,7 @@ export class ServerService {
 
   // we make handleError private because it's supposed to only be inside of this class
   // handleError method used for debugging purposes, whenever we're debugging this appliation or running it so that we can see the error in the console, if something happens
-  private handleError(error: HttpErrorResponse): Observable<never> {
+  handleError(error: HttpErrorResponse): Observable<never> {
     console.log(error)
     return throwError('An error occurred. Error code: ${error.status}');
   }
